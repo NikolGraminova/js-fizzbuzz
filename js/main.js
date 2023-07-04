07adca9cb37b0e1c5143b1e40047c965b0ce0729
NikolGraminova <nbackup619@gmail.com>
2023-07-04 14:43:01 +0200

    Added main operation

--- js/main.js
+++ js/main.js
@@ -12,3 +12,15 @@ BONUS 1
 Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta, così come fatto in classe questa mattina.
 BONUS 2
 Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.*/
+
+
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+    if (i % 3 == 0) {
+        console.log("Fizz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz")
+    } else if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
+    }
+}
